d237052f60674597becf7a8679f5b8a0cdef8bfd
MohamedAyman360 <moh.ayman1999@gmail.com>
2025-04-07 23:09:16 +0200

    change url

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -8,7 +8,7 @@ const ListProduct = () => {
     const [allproducts,setAllProducts] = useState([]);
 
     const fetchInfo = async ()=>{
-      await fetch('http://localhost:4000/allproducts')
+      await fetch('https://e-commercefrontend-gray.vercel.app/allproducts')
       .then((res)=> res.json())
       .then((data)=>{setAllProducts(data)});
     }
@@ -17,7 +17,7 @@ const ListProduct = () => {
       fetchInfo();
     },[])
     const remove_product = async (id)=>{
-      await fetch('http://localhost:4000/removeproduct', {
+      await fetch('https://e-commercefrontend-gray.vercel.app/removeproduct', {
         method:'POST',
         headers:{
           Accept:'application/json',
