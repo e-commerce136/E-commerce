937e06bf6152b24e862f1e731ddd0aa2e3da7830
MohamedAyman360 <moh.ayman1999@gmail.com>
2025-04-07 23:03:06 +0200

    change route url

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -20,12 +20,12 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
     
     useEffect(()=> {
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://e-commercebackend-jade.vercel.app/allproducts')
         .then((response)=> response.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch('https://e-commercebackend-jade.vercel.app/getcart',{
             method:'POST',
             headers:{
                 Accept:'application/form-data',
@@ -41,7 +41,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId] +1}));
         if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/addtocart',{
+            fetch('https://e-commercebackend-jade.vercel.app/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -58,7 +58,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId] -1}));
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://e-commercebackend-jade.vercel.app/removefromcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
